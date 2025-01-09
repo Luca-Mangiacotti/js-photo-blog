ad010ae7c6c20f38d38232f85527424e849ffc21
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-01-09 17:03:34 +0100

    feat add JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 //FUNCTIONS
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
-.then(function(addPic){
-    console.log(addPic.data)
+.then((addCard) => {
+
+    console.log(addCard.data)
+    const cardData = addCard.data
+    console.log(cardData)
+
+    for(let i = 0; i < cardData.length; i++ ){
+        console.log(cardData[i])
+        blogElm.innerHTML += `<div class="card">
+                                  <img class="img-fluid" src="${cardData[i].url}" alt="imgcard">
+                                  <p>${cardData[i].title}</p>
+                                  <div class="nailPic"><img src="./img/pin.svg" alt="chiodino"></div>
+                              </div>`
+    }
+    
+    //test
+    // addCard.forEach(cardElm => {
+
+    //     const currentCard = addCard.data
+    //     blogElm.innerHTML += `<div class="card">
+    //                              <img class="img-fluid" src="${currentCard.url}" alt="imgcard">
+    //                              <p>${currentCard.title}</p>
+    //                              <div class="nailPic"><img src="./img/pin.svg" alt="chiodino"></div>
+    //                           </div>`
+    //     console.log(currentCard.title)
+    
+    // })
 })
 
 //DOM ELEMENTS
